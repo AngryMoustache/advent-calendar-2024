4cf60ae66ddc62ae020bd0e6210b2dd2fd5f71ad
AngryMoustache <sander.vandamme11@gmail.com>
2024-12-01 20:39:15 +0100

    Setup routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+for ($i = 1; $i <= 24; $i++) {
+    Route::get("/day-{$i}/one", "\App\Http\Controllers\Day{$i}Controller@one");
+    Route::get("/day-{$i}/two", "\App\Http\Controllers\Day{$i}Controller@two");
+}
