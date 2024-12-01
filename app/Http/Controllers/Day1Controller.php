a37e1c3c7d025096e05cc68f5195d34b64aec877
AngryMoustache <sander.vandamme11@gmail.com>
2024-12-01 20:53:17 +0100

    Day 1

--- app/Http/Controllers/Day1Controller.php
+++ app/Http/Controllers/Day1Controller.php
@@ -8,18 +8,32 @@ class Day1Controller extends Controller
 {
     public function one()
     {
+        $lists = $this->data();
 
+        return $lists[0]->keys()
+            ->map(fn (int $index) => abs($lists[0][$index] - $lists[1][$index]))
+            ->sum();
     }
 
     public function two()
     {
+        $lists = $this->data();
 
+        return $lists[0]->reduce(fn (int $carry, int $value) => $carry + abs(
+            $value * ($lists[1]->countBy()[$value] ?? 0)
+        ), 0);
     }
 
     private function data()
     {
         $file = File::get(public_path('inputs/1-1.txt'), 'r');
 
-        return collect(explode(PHP_EOL, $file))->filter();
+        $lists = collect(explode(PHP_EOL, $file))
+            ->filter()
+            ->map(fn (string $line) => explode('   ', $line));
+
+        return collect(range(0, 1))->map(fn (int $index) =>
+            $lists->pluck($index)->map(fn (int $number) => $number)->sort()->values()
+        );
     }
 }
